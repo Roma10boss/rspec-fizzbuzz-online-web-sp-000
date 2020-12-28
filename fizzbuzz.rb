1e386525a6c63b919c609a78fb03f8672e03787b
Romario Gustave <romagustave10@gmail.com>
2020-12-28 20:13:40 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,36 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "FizzBuzz" # Go fizz
+require 'rspec'
+
+def fizzBuzz(number)
+  if number % 3 == 0 && number % 5 == 0
+    "FizzBuzz"
+  elsif number % 3 == 0
+    "Fizz"
+  elsif number % 5 == 0
+    "Buzz"
+  else
+    number
   end
 end
+
+RSpec.describe "Fizz Buzz Tests" do
+  it "should return `Fizz` for number divisible by 3" do
+    expect(fizzBuzz(3)).to eq "Fizz"
+  end
+
+  it "should return `Buzz` for number divisible by 5" do
+    expect(fizzBuzz(5)).to eq "Buzz"
+  end
+
+  it "should return `FizzBuzz` for number divisible by 3 and 5" do
+    expect(fizzBuzz(15)).to eq "FizzBuzz"
+  end
+
+  it "should return `FizzBuzz` for number divisible by 3 and 5" do
+    expect(fizzBuzz(90)).to eq "FizzBuzz"
+  end
+
+  it "should return 7 since it is indivisible by 3 and 5" do
+    expect(fizzBuzz(7)).to eq 7
+  end
